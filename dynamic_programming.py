d2b887dbb9995e6e08faccb3fae3575e9eafad00
Benjamin Swartz <benjaminswartz@Benjamins-MacBook-Air-2.local>
2026-05-21 19:43:20 -0400

    More environment work

---
+++ dynamic_programming.py
@@ -0,0 +1,17 @@
+def value_function(env, theta=1e-5):
+    """
+    Find the optimal value function using an iterative strategy
+    
+    Returns a dictionary of the expected reward of being in each state
+    """
+    pass
+
+# Use the value function
+def Q_function(env, V, theta=1e-5):
+    """
+    Using the value function, greedily choose the optimal action in each state.
+    This will the optimal policy for the environment.
+    
+    Returns a dictionary containing a policy table for the optimal action in each state.
+    """
+    pass
--- environment.py
+++ environment.py
@@ -15,11 +15,11 @@ class GridWorldEnv:
 
         self.states = list(range(self.n_rows * self.n_cols))
         self.actions = ["LEFT", "RIGHT", "UP", "DOWN"]
-
-        self.start_state = start_state
+        self.start_state = self.find_start_state()
         self.state = start_state
         self.gamma = gamma
         self.is_slippery = is_slippery
+        self.terminal_states = self.get_terminal_states()
 
         self.terminal_states = self.get_terminal_states()
     def state_to_pos(self, state):
@@ -75,6 +75,107 @@ class GridWorldEnv:
             if tile in ["H", "G"]:
                 terminal_states.append(s)
         return terminal_states
-                
-            
-            
+    def is_terminal(self, state):
+        """
+        Returns True if state is terminal, False otherwise.
+        
+        """
+        return state in self.terminal_states
+    def reward(self, next_state):
+        """
+        Return reward fo entering next_state
+
+        - Entering goal gives +1
+        - Everything else gives 0
+        
+        """
+        reward = 0
+        next_tile = self.get_tile(next_state)
+        if next_tile == "G":
+            reward = 1
+            return reward
+        return reward
+    def reset(self):
+        """
+        Reset agent back to start state.
+
+        Return starting state
+
+        """
+        self.state = self.start_state
+        return self.state
+    def move(self, state, action):
+        """
+        Deterministically move from state to state according to action.
+        Actions:
+        - LEFT
+        - RIGHT
+        - UP 
+        - DOWN
+
+        Keep the agent inside the grid
+        
+        """
+        row, col = self.state_to_pos(state)
+        # Up increases row
+        if action == "UP":
+            row -= 1
+        # Down decreases row
+        elif action == "DOWN":
+            row += 1
+        # Left increases column
+        elif action == "RIGHT":
+            col += 1
+        elif action == "LEFT":
+            col -= 1
+        # I need to clamp row and col so they stay inside of the grid
+        row = max(0, min(row, self.n_rows - 1))
+        col = max(0, min(col, self.n_rows - 1))
+        next_state = self.pos_to_state(row, col)
+        
+        return next_state
+
+    def get_slippery_actions(self, action):
+        """
+        Returns the possible actual actions caused by slipping.
+        """
+
+        slippery_actions = {
+        "LEFT": ["UP", "LEFT", "DOWN"],
+        "RIGHT": ["UP", "RIGHT", "DOWN"],
+        "UP": ["LEFT", "UP", "RIGHT"],
+        "DOWN": ["LEFT", "DOWN", "RIGHT"]
+        }
+
+        return slippery_actions[action] # List of actions caused by slipping
+    def get_transitions(self, state, action):
+        """ 
+        Return possible transitions from state after choosing action.
+
+        Return:
+        [(probability, next_state), ...]
+
+        If the state is terminal:
+            return [(1.0, state)]
+        If not slippery:
+            return one transition with probability 1.0.
+        If slippery:
+            Each actual action has probability 1/3.
+        """
+        if self.is_terminal(state) == True:
+            return [(1.0, state)]
+        if self.is_slippery == False:
+            next_state = self.move(state, action)
+            return [(1.0, next_state)]
+        slippery_actions = self.get_slippery_actions(action)
+        transitions = []
+        for action in slippery_actions:
+            transitions.append((0.33, action))
+        return transitions
+    
+        
+        
+
+
+env = GridWorldEnv()
+print(env.get_transitions(4, "DOWN"))
